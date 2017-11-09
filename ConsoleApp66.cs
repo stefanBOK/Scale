0294d84566fa042b524c117f866e5760aae914e4
stefanBOK <32751286+stefanBOK@users.noreply.github.com>
2017-11-09 17:04:53 +0200

    Add files via upload

---
+++ ConsoleApp66.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 1va
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        while (true)
+        {
+            int a;
+            double p;
+            a = Convert.ToInt32(Console.ReadLine());
+            p = (a + 1) / 2;
+            Console.WriteLine(Math.Round(p));
+        }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 7ma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        while (true)
+        {
+            int a;
+            a = Convert.ToInt32(Console.ReadLine());
+            for int (i = 0;
+                i <= a;
+                i++)
+            {
+                for (int x = 0; x < i; x++)
+                {
+                    Console.ReadLine('$');
+                }
+                Console.ReadLine();
+    
+        }
+    }
+}
